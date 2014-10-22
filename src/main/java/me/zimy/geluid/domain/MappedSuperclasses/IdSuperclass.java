7e2044688b0edf9aaa6c0272c8f6ddd1d48659aa
zimy <zimy@yandex.ru>
2014-10-22 22:35:33 +0400

    Added getter and setter for IdSuperclass

--- src/main/java/me/zimy/geluid/domain/MappedSuperclasses/IdSuperclass.java
+++ src/main/java/me/zimy/geluid/domain/MappedSuperclasses/IdSuperclass.java
@@ -16,6 +16,14 @@ public class IdSuperclass implements Serializable {
     long id;
     String name;
 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
     public long getId() {
         return id;
     }
