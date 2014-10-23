f4d142867d3eafec66815a16857e5aac3fb4fc56
zimy <zimy@yandex.ru>
2014-10-23 04:54:14 +0400

    Dmitriy Eremin added extension finding algorithm

---
+++ src/main/java/me/zimy/geluid/scanning/ExtensionFinder.java
@@ -0,0 +1,15 @@
+package me.zimy.geluid.scanning;
+
+/**
+ * @author Dmitriy &lt;Shnay&gt; Eremein
+ */
+public class ExtensionFinder {
+    public static String getExtension(String fileName) {
+        String extension = "";
+        int i = fileName.lastIndexOf('.');
+        if (i > 0) {
+            extension = fileName.substring(i + 1);
+        }
+        return extension;
+    }
+}
