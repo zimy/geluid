b3773566185b488d90e81ea0f047fb4892ca1f85
zimy <zimy@yandex.ru>
2014-10-23 04:44:59 +0400

    Authors controller added

---
+++ src/main/java/me/zimy/geluid/controllers/AuthorController.java
@@ -0,0 +1,52 @@
+package me.zimy.geluid.controllers;
+
+import me.zimy.geluid.domain.Author;
+import me.zimy.geluid.repositories.AuthorRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+/**
+ * @author Dmitriy &lt;Zimy(x)&gt; Yakovlev
+ */
+@Controller
+@RequestMapping("/authors")
+public class AuthorController {
+    @Autowired
+    private AuthorRepository repository;
+
+    @ResponseBody
+    @RequestMapping(method = RequestMethod.GET)
+    public List<Author> getAll() {
+        return repository.findAll();
+    }
+
+    @ResponseBody
+    @RequestMapping(value = "/{id}", method = RequestMethod.GET)
+    public Author getOneByURIRequest(@PathVariable Long id) {
+        return repository.findOne(id);
+    }
+
+    @ResponseBody
+    @RequestMapping(method = RequestMethod.GET, params = {"name"})
+    public Author getOneByRequestParam(@RequestParam(value = "id", defaultValue = "0") Long id) {
+        return repository.findOne(id);
+    }
+
+    @ResponseBody
+    @RequestMapping(method = RequestMethod.POST)
+    public Author post(@RequestParam String name) {
+        Author author = new Author();
+        author.setName(name);
+        return repository.saveAndFlush(author);
+    }
+
+    @ResponseBody
+    @RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
+    public String removeOne(@PathVariable Long id) {
+        repository.delete(id);
+        return "OK";
+    }
+}
