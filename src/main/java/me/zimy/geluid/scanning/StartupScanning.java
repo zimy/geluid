33540652b0e8d7041d6c1e0c844d4190a3ad8a9f
Dmitriy Zimy Yakovlev <zimy@yandex.ru>
2014-12-14 18:15:38 +0300

    Now using correct way to perform startup scanning

--- src/main/java/me/zimy/geluid/scanning/StartupScanning.java
+++ src/main/java/me/zimy/geluid/scanning/StartupScanning.java
@@ -3,6 +3,7 @@ package me.zimy.geluid.scanning;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import javax.annotation.PostConstruct;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -11,11 +12,11 @@ import java.util.logging.Logger;
  */
 @Component
 public class StartupScanning {
+    @Autowired
     GeluidScanner geluidScanner;
 
-    @Autowired
-    public void setGeluidScanner(GeluidScanner geluidScanner) {
-        this.geluidScanner = geluidScanner;
+    @PostConstruct
+    public void setGeluidScanner() {
         Logger.getLogger("org.jaudiotagger").setLevel(Level.OFF);
         geluidScanner.setDepth(10);
         geluidScanner.run();
